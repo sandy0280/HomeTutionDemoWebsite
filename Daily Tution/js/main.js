d82bd3dd7b33459c44125f55e23821e002002c77
SANDEEP MISHRA <58391925+sandy0280@users.noreply.github.com>
2020-07-24 13:24:25 +0530

    Add files via upload
    initial Commit

---
+++ Daily Tution/js/main.js
@@ -0,0 +1,9 @@
+
+$('.slider-one').not(".slick-initialized")
+.slick({
+        autoplay: true,
+        autoplaySpeed : 2000,
+        dots: true,
+        prevArrow : ".site-slider.slider-btn.prev",
+        nextArrow : ".site-slider.slider-btn.next"
+});
